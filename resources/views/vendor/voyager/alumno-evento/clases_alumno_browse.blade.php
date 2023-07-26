f029af41f5bfe5381606176edbf159eb01311c82
albamaidana <a_maidana@gestionsolutions.com.ar>
2023-07-26 17:50:41 -0300

    agrega columna clases en informes asistencia -planillas por profesor y seguimiento

--- resources/views/vendor/voyager/alumno-evento/clases_alumno_browse.blade.php
+++ resources/views/vendor/voyager/alumno-evento/clases_alumno_browse.blade.php
@@ -20,12 +20,14 @@
         </th>
         <th>Alumno</th>
         <th>Curso</th>
-        <th>Fecha</th>
-        <th>Descripcion Clase</th>
+        <th>Fecha y horario</th>
+        <th>Franja Horaria</th>
+        <th>Nro Clase</th>
+        <th>Observaciones Clase</th>
         <th>Tipo Clase</th>
         <th>Instructor</th>
         <th>Asistencia</th>
-        <th>Franja Horaria</th>
+        
         </tr>
      </thead>
      
@@ -56,11 +58,13 @@
                      {data: 'nombre_alumno', name: 'alumnos.nombre', width: '10%'},
                      {data: 'nombre_curso', name: 'cursos.nombre_curso', width: '10%'},
                      {data: 'fecha', name: 'alumno_evento.start_date', width: '10%'},
-                     {data: 'clase', name: 'alumno_evento.descripcion', width: '10%'},
+                     {data: 'descripcion', name: 'franjas_horarias.descripcion', width: '10%'},
+                     {data: 'clase', name: 'alumno_evento.clase', width: '10%'},
+                     {data: 'observaciones', name: 'alumno_evento.descripcion', width: '10%'},
                      {data: 'tipo_evento', name: 'tipos_eventos.tipo_evento', width: '10%'},
                      {data: 'nombre_instructor', name: 'instructores.nombre', width: '10%'},
                      {data: 'asistencia', name: 'alumno_evento.asistencia', width: '10%'},
-                     {data: 'descripcion', name: 'franjas_horarias.descripcion', width: '10%'},
+                     
                         ]           
         } );
 
